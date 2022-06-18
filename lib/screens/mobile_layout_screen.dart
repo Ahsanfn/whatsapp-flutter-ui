86d3e06a216ed4762a355bf09bbed4b48c40e9c4
Ahsan <79699555+Ahsanfn@users.noreply.github.com>
2022-06-18 22:19:44 +0500

    a
    a

---
+++ lib/screens/mobile_layout_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+// ignore: unused_import
+import 'package:whatsapp_ui/colors.dart';
+
+class MobileLayoutScreen extends StatelessWidget {
+  const MobileLayoutScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Text('Mobile!'),
+    );
+  }
+}
